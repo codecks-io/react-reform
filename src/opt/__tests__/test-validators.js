3f1d20d238d9b240df5b49538650c7a6aabb3b36
Daniel Berndt <daniel.berndt@gmail.com>
2016-01-25 11:31:26 +0100

    fix .gitignore

---
+++ src/opt/__tests__/test-validators.js
@@ -0,0 +1,66 @@
+/*eslint-env mocha*/
+
+import expect from "expect";
+import "react-reform/opt/validators";
+import {getValidator} from "react-reform/validator-store";
+
+describe("Default Validators", function() {
+
+  function checkValidator(validator, target, input, opts) {
+    expect(validator.isValid(input, {opts})).toBe(target, `(${input}${opts !== undefined ? `, ${opts}` : ""}) should be ${target}`);
+  }
+
+  it("should work for required", function() {
+    const validator = getValidator("required");
+    checkValidator(validator, true, "foo");
+    checkValidator(validator, false, "");
+    checkValidator(validator, false, " ");
+    checkValidator(validator, false, "  \n\t  \n");
+    checkValidator(validator, true, true);
+    checkValidator(validator, false, false);
+    checkValidator(validator, false, null);
+    checkValidator(validator, false, undefined);
+    checkValidator(validator, true, 0);
+  });
+
+  it("should work for email", function() {
+    const validator = getValidator("email");
+    checkValidator(validator, false, "foo");
+    checkValidator(validator, true, "foo@bar.com");
+    checkValidator(validator, true, "foo@bar.fancynewtld");
+    checkValidator(validator, false, "foo@bar");
+    checkValidator(validator, false, null);
+  });
+
+  it("should work for minlength", function() {
+    const validator = getValidator("minlength");
+    checkValidator(validator, true, "foo", 2);
+    checkValidator(validator, true, "foo", 3);
+    checkValidator(validator, false, "foo", 4);
+    checkValidator(validator, false, "foo", 4);
+    checkValidator(validator, false, null, 4);
+    checkValidator(validator, false, 1, 4);
+    checkValidator(validator, true, 1000, 4);
+  });
+
+  it("should work for maxlength", function() {
+    const validator = getValidator("maxlength");
+    checkValidator(validator, true, "foo", 3);
+    checkValidator(validator, true, "foo", 4);
+    checkValidator(validator, false, "foo", 2);
+    checkValidator(validator, true, null, 4);
+    checkValidator(validator, true, 1, 4);
+    checkValidator(validator, false, 10000, 4);
+  });
+
+  it("should work for pattern", function() {
+    const validator = getValidator("pattern");
+    checkValidator(validator, true, "5", "\\d+");
+    checkValidator(validator, true, "5", /\d+/);
+    checkValidator(validator, true, "5a", /\d+/);
+    checkValidator(validator, false, "5a", /^\d+$/);
+    checkValidator(validator, false, null, /\d+/);
+  });
+
+
+});
---
+++ src/opt/theme-minimal.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default (FormContainer, Fields, {globalErrors, submitForm}) => (
+  <FormContainer>
+    {globalErrors.length ? (
+      globalErrors.map((error, i) => (
+        <div key={i} style={{color: "red"}}>{error}</div>
+      ))
+    ) : null}
+    <Fields>
+      {(Field, {label, validations, isTouched, hasFailedToSubmit, fieldProps, id}) => {
+        const validationList = validations
+          .filter(({isValid}) => fieldProps.showHints || ((isTouched || hasFailedToSubmit) && isValid !== true))
+          .map(({isValid, errorMessage, hintMessage, type}) => {
+            if (isValid === false) return {message: errorMessage, type};
+            if (isValid === true) return {message: hintMessage, type};
+            return {message: isValid, type};
+          })
+          .map(({type, message}) => <span key={type} style={{color: "red"}}>{message}</span>);
+        return (
+          <div>
+            <label htmlFor={id}>{label}</label>
+            <Field id={id}/>
+            {validationList}
+          </div>
+        );
+      }}
+    </Fields>
+    <button>Submit</button>
+  </FormContainer>
+);
